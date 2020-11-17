335bd1779ad69d953660d05b131285c691b531f4
genjuro1444 <genjuro1444@163.com>
2020-11-17 15:01:01 +0800

    初始化

--- script/config.js
+++
@@ -1,6 +0,0 @@
-CONFIG = {
-    //apiurl: 'http://192.168.0.100/API/MobileAPI.ashx',
-    //url: 'http://192.168.0.100/'
-      apiurl: 'http://118.122.8.131:8001/API/MobileAPI.ashx',
-      url: 'http://118.122.8.131:8001'
-};
