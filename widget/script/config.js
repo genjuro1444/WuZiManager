af27c5faddce6412675e19508311684d2e048b83
genjuro1444 <genjuro1444@163.com>
2020-11-18 10:47:23 +0800

    修改

--- widget/script/config.js
+++ widget/script/config.js
@@ -1,6 +1,8 @@
 CONFIG = {
     //apiurl: 'http://192.168.0.100/API/MobileAPI.ashx',
     //url: 'http://192.168.0.100/'
-      apiurl: 'http://118.122.8.131:8001/API/MobileAPI.ashx',
-      url: 'http://118.122.8.131:8001'
+    apiurl: 'http://192.168.182.212/API/MobileAPI.ashx',
+    url: 'http://192.168.182.212'
+      //apiurl: 'http://118.122.8.131:8001/API/MobileAPI.ashx',
+      //url: 'http://118.122.8.131:8001'
 };
