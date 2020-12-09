7cc923f01fe2f8ddebc9e5c83f9ff9040f14988b
genjuro1444 <genjuro1444@163.com>
2020-12-09 22:37:20 +0800

    123

--- widget/script/config.js
+++ widget/script/config.js
@@ -1,6 +1,6 @@
 CONFIG = {
-    apiurl: 'http://192.168.3.127/API/MobileAPI.ashx',
-    url: 'http://192.168.3.127/'
+    apiurl: 'http://192.168.0.102/API/MobileAPI.ashx',
+    url: 'http://192.168.0.102/'
       //apiurl: 'http://www.lstobacco.com/API/MobileAPI.ashx',
       //url: 'http://www.lstobacco.com'
     //apiurl: 'http://118.122.8.131:8001/API/MobileAPI.ashx',
